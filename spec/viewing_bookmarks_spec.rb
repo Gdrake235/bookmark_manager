2be3c52feb00f0b413f8f5d99db17af3033fe46b
Student <student@Makerss-MacBook-Air-2.local>
2020-03-10 16:28:52 +0000

    exercise 9

--- spec/viewing_bookmarks_spec.rb
+++ spec/viewing_bookmarks_spec.rb
@@ -1,16 +1,25 @@
-feature 'viewing_bookmarks' do
-feature 'viewing index' do
-  scenario 'visiting index page' do
-    visit('/')
-    expect(page).to have_content "Bookmark Manager"
+feature 'Viewing bookmarks' do
+  feature 'visiting the homepage' do
+    scenario 'the page title is visible' do
+      visit '/'
+      expect(page).to have_content 'Bookmark Manager'
+    end
   end
-end
-feature 'viewing bookmarks' do
-  scenario 'displays bookmarks' do
-    visit('/bookmarks')
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
+
+  feature 'viewing bookmarks' do
+    scenario 'bookmarks are visible' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+  
+      connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
+      visit '/bookmarks'
+
+      expect(page).to have_content 'http://www.makersacademy.com'
+      expect(page).to have_content 'http://www.destroyallsoftware.com'
+      expect(page).to have_content 'http://www.google.com'
+    end
   end
-end
 end
